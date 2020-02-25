d96a6f57dba37fb7f53387c16790a313911cab2d
xiehaibin18@gmail.com <xiehaibin18@gmail.com>
2020-02-25 15:33:46 +0800

    userregister finished

---
+++ controller/client/UserRegister.js
@@ -0,0 +1,47 @@
+const query = require('../mysql')
+
+  /** 生成返回UID
+   * @param {string} number 手机号码
+   */
+  function createUID(number) {
+    // 获取日期信息
+    let date = new Date()
+    let UID = null
+    let Y = date.getFullYear()
+    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1)
+    let D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate())
+    date = `${Y}${M}${D}`
+    // 合并手机号码和日期信息生成UID
+    return UID = `${number}${date}`
+  }
+
+module.exports = function (data, callback) {
+  // 检查手机号码是否注册
+  query(`SELECT COUNT(*) FROM personal_info WHERE personal_uid LIKE '${data.phone}%'`)
+  .then(res => {
+    res = JSON.parse(res)
+    if (res[0]['COUNT(*)'] > 0) {
+      callback(null, { 'err_code': 1, 'message': '手机号码已注册' })
+    } else {
+      // 获取UID
+      UID = createUID(data.phone)
+      // 字段名及值
+      let column_name = `personal_uid,personal_account,personal_password,personal_nickname`
+      let VALUES = `'${UID}','${data.account}','${data.password}','${data.nickname}'`
+      query(`INSERT INTO personal_info(${column_name}) VALUES (${VALUES})`)
+        .then(() => {
+          let account_token = new Date().getTime() + 259200000;
+          query(`UPDATE personal_info SET account_token="${data.account}-${account_token}" WHERE personal_account="${data.account}" AND personal_password="${data.password}"`)
+          callback(null, { 'err_code': 0, 'account_token': `${data.account}-${account_token}` })
+        })
+        .catch(err => {
+          callback(err, null)
+          console.log(err)
+        })
+    }
+  })
+  .catch(err => {
+    callback("验证手机号码失败", null)
+    console.log(err)
+  })
+}
--- router/client/Personal.js
+++ router/client/Personal.js
@@ -2,15 +2,30 @@ const express = require('express')
 
 const CheckAccountToken = require('../../controller/client/CheckAccountToken')
 const CheckLogin = require('../../controller/client/CheckLogin')
+const UserRegister = require('../../controller/client/UserRegister')
 
 const router = express.Router()
 
 router
   .post('/api/CheckAccountToken', (req, res) => {
     if (!req.body.accountToken) {
-      res.status(400).json({"err_code": 1,"msg":"数据传输失败"})
+      res.status(400).json({"err_code": 400,"msg":"数据传输失败"})
     } else {
       CheckAccountToken(req.body.accountToken, (err, data) => {
+        if (data) {
+          res.status(200).json(data)
+        } else {
+          res.status(500).json({"err_code": 500,"msg":err})
+        }
+      })
+    }
+  })
+  .post('/api/CheckLogin', (req, res) => {
+    console.log(req.body)
+    if (!req.body.account || !req.body.password) {
+      res.status(400).json({"err_code": 1,"msg":"数据传输失败"})
+    } else {
+      CheckLogin(req.body, (err, data) => {
         if (data) {
           res.status(200).json(data)
         } else {
@@ -19,11 +34,11 @@ router
       })
     }
   })
-  .post('/api/CheckLogin', (req, res) => {
-    if (!req.body.account && !req.body.password) {
-      res.status(400).json({"err_code": 1,"msg":"数据传输失败"})
+  .post('/api/UserRegister', (req, res) => {
+    if (!req.body.account || !req.body.password || !req.body.nickname || !req.body.phone) {
+      res.status(400).json({"err_code": 400,"msg":"数据传输失败"})
     } else {
-      CheckLogin(req.body, (err, data) => {
+      UserRegister(req.body, (err, data) => {
         if (data) {
           res.status(200).json(data)
         } else {
