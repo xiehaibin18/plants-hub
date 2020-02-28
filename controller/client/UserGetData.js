922b1daf1229a2e45a3c0762a5741f76295c2a65
xiehaibin18@gmail.com <xiehaibin18@gmail.com>
2020-02-28 23:44:29 +0800

    client userLogged finished

--- controller/client/UserGetData.js
+++ controller/client/UserGetData.js
@@ -6,16 +6,27 @@ module.exports = function (data, callback) {
     // column = `SELECT  FROM plants_info p,location_info l WHERE p.plants_distributions_uid = l.location_uid`
     column = `SELECT plants_uid,plants_name,plants_introduction,plants_picture,plants_distributions_uid,plants_like FROM plants_info`
   }
+  if (data.type == 'getUserInfo') {
+    column = `SELECT personal_nickname,personal_avatar FROM personal_info WHERE account_token='${data.accountToken}'`
+  }
   query(column)
-    .then(data => {
-      data = JSON.parse(data)
-      data.forEach(element => {
-        element.plants_picture = `http://192.168.0.105:3000${element.plants_picture}`
-      });
-      callback(null, { 'err_code': 2, 'data': data })
+    .then(res => {
+      res = JSON.parse(res)
+      if (data.type == 'allPlants') {
+        res.forEach(element => {
+          element.plants_picture = `http://192.168.0.105:3000${element.plants_picture}`
+        });
+      }
+      if (data.type == 'getUserInfo') {
+        res.forEach(element => {
+          element.personal_avatar = `http://192.168.0.105:3000${element.personal_avatar}`
+        });
+      }
+      callback(null, { 'err_code': 0, 'data': res })
     })
     .catch(err => {
       callback(err, null)
+      console.log(err)
     })
 
 }
