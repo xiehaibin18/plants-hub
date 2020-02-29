7b8bb051255afa4e293ae6a607a1862ed46d9cf5
xiehaibin18@gmail.com <xiehaibin18@gmail.com>
2020-02-29 16:57:38 +0800

    client checkLogin fixBug

--- controller/client/CheckLogin.js
+++ controller/client/CheckLogin.js
@@ -7,19 +7,20 @@ module.exports = function (data, callback) {
       if (JSON.parse(res)[0]['COUNT(*)'] == 0) {
         return callback(null, { 'err_code': 2, 'message': '账户错误' })
       } else {
-        return query(`SELECT COUNT(*) FROM personal_info WHERE personal_account="${data.account}" AND personal_password="${data.password}"`)
+        return query(`SELECT personal_uid FROM personal_info WHERE personal_account="${data.account}" AND personal_password="${data.password}"`)
       }
     }, err => {
       return callback(err, null)
     })
     .then(res => {
-      if (!res) {return}
-      if (JSON.parse(res)[0]['COUNT(*)'] == 0) {
+      if (!res) {return};
+      if (JSON.parse(res).length == 0) {
         return callback(null, { 'err_code': 3, 'message': '密码错误' })
       } else {
-        let account_token = new Date().getTime() + 259200000;
-        query(`UPDATE personal_info SET account_token="${data.account}-${account_token}" WHERE personal_account="${data.account}" AND personal_password="${data.password}"`)
-        callback(null, { 'err_code': 0, 'account_token': `${data.account}-${account_token}` })
+        let lots = new Date().getTime() + 259200000;
+        let phone = JSON.parse(res)[0].personal_uid.slice(0,11)
+        query(`UPDATE personal_info SET account_token="${phone}-${lots}" WHERE personal_account="${data.account}" AND personal_password="${data.password}"`)
+        callback(null, { 'err_code': 0, 'account_token': `${phone}-${lots}` })
       }
     }, err => {
       return callback(err, null)
